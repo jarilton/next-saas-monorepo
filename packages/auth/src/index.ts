8a829385a6cd3359fbd697d81e860b32dc65339e
jariltonpereira <jarilton.pereira-terceiro@cea.com.br>
2025-08-21 15:55:24 -0300

    feat: criado as permissões por cargo

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,6 +1,9 @@
 import type { CreateAbility, ForcedSubject, MongoAbility } from '@casl/ability'
 import { AbilityBuilder, createMongoAbility } from '@casl/ability'
 
+import type { User } from './models/user'
+import { permissions } from './permissions'
+
 const actions = ['manage', 'invite', 'delete'] as const
 const subjects = ['User', 'all'] as const
 
@@ -15,9 +18,16 @@ type AppAbilities = [
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
 
-const { build, can, cannot } = new AbilityBuilder(createAppAbility)
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
 
-can('invite', 'User')
-cannot('delete', 'User')
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permission for role ${user.role} not found`)
+  }
 
-export const ability = build() as AppAbility
+  permissions[user.role](user, builder)
+
+  const ability = builder.build() as AppAbility
+
+  return ability
+}
