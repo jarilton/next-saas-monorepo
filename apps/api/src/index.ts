fa45d748323d7ecab899646cd2c05990ae72d8d8
jariltonpereira <jarilton.pereira-terceiro@cea.com.br>
2025-08-25 23:57:03 -0300

    fix: ajustes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,13 +3,10 @@ import { defineAbilityFor, projectSchema } from '@saas/auth'
 const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-1' })
 
 const project = projectSchema.parse({
-  id: 'project-1',
-  name: 'Project 1',
-  description: 'Description for Project 1',
+  id: 'user-1',
   ownerId: 'user-1',
 })
 
 console.log(ability.can('get', 'Billing'))
 console.log(ability.can('create', 'Invite'))
-console.log(ability.can('update', 'Project'))
-console.log(ability.can('delete', 'Project'))
+console.log(ability.can('delete', project))
